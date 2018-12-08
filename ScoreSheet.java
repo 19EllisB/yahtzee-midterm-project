06668abc4005c354cb4080beb409d4f414ce771d
Brent <19ellisb@flintridgeprep.org>
2018-12-07 22:54:17 -0800

    finished tally

--- ScoreSheet.java
+++ ScoreSheet.java
@@ -4,7 +4,7 @@ public class ScoreSheet {
 
     public void clear() {//resets all values to default
         /*for(int i: scores) { this loop does NOT work for some reason
-            i = 0;
+        i = 0;
         }*/
         for (int i = 0; i < scores.length; i++) { //this loop is functional.
             scores[i] = 0;
@@ -13,13 +13,30 @@ public class ScoreSheet {
 
     public void tally() {
         int upper = 0;
+        int lower = 0;
+        
+        //upper tally
         for (int i = 0; i < 6; i++) {
             upper += scores[i];
         }
-       
+        if (upper >= 63) { //condition for upper bonus; 35 pts.
+            upper += 35;
+            scores[ScoreLabel.UPPER_SECTION_BONUS.ordinal()] = 35;
+        }
+        scores[ScoreLabel.UPPER_SECTION_TOTAL.ordinal()] = upper;
+        
+        //lower tally
+        for (int i = 8; i < 16; i++) { //range of the lower section
+            lower += scores[i];
+        }
+        scores[ScoreLabel.LOWER_SECTION_TOTAL.ordinal()] = lower;
+        
+        //grand tally
+        scores[ScoreLabel.GRAND_TOTAL.ordinal()] = upper + lower;
     }
-    
+
     public void print() {
+        tally(); //included so that tallying is implicit w/ printing
         for (ScoreLabel s: ScoreLabel.values()) { //Iterates across all values of the enum ScoreLabel
             //s.ordinal() returns the index of s in the ScoreLabel list
             //this.scores[s.ordinal()] is the score in scores associated with each ScoreLabel
@@ -27,5 +44,5 @@ public class ScoreSheet {
         }
         System.out.println("");
     }
-    
+
 }
--- ScoreSheetTestDrive.java
+++ ScoreSheetTestDrive.java
@@ -14,5 +14,15 @@ public class ScoreSheetTestDrive {
         //test clear()
         sheet1.clear();
         sheet1.print();
+        
+        //test tally
+        sheet1.scores[0] = 1;
+        sheet1.scores[10] = 25;
+        sheet1.tally();
+        sheet1.print();
+        //bonus application test
+        sheet1.scores[1] = 200;
+        sheet1.tally();
+        sheet1.print();
     }
 }
